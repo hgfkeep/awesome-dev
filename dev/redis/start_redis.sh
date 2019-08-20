cce522df9ad8590f1a8f878bf00a898f6c39b53e
guangfu he <guangfuhe@guangfudeMacBook-Pro.local>
2019-08-20 15:22:04 +0800

    ADD redis and zookeeper quoram cluster

--- dev/redis/start_redis.sh
+++ dev/redis/start_redis.sh
@@ -2,4 +2,4 @@
 
 docker stop redis_dev
 docker rm redis_dev
-docker run -itd  -p 6379:6379 -v `pwd`/data:/data --name redis_dev  redis:latest
+docker run -d  -p 6379:6379 -v `pwd`/data:/data -v `pwd`/conf/redis.conf:/usr/local/etc/redis/redis.conf --name redis_dev  redis:latest redis-server /usr/local/etc/redis/redis.conf
