33632618074bd39175b99e592ab0b59003e70166
hgfkeep <hgfkeep@gmail.com>
2020-05-21 10:40:50 +0800

    👌 Improve: 增加minio 资源限制

--- dev/minio/start_minio.sh
+++ dev/minio/start_minio.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-
+docker stop minio
 docker rm minio
 
-docker run -p 2345:9000 --name minio \
+docker run -p 2345:9000 --name minio --restart=always \
+  -m 200m --memory-swap -1 \
   -e "MINIO_ACCESS_KEY=hgfgood" \
   -e "MINIO_SECRET_KEY=hgfgooda" \
   -v `pwd`/data:/data \
