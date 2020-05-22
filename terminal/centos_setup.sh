f9b5475db5088ea194f2f6bec50a59771d702a84
hgfkeep <hgfkeep@gmail.com>
2020-05-22 17:59:25 +0800

    👌 Improve: 改进linux中brew 的环境变量配置

--- terminal/centos_setup.sh
+++ terminal/centos_setup.sh
@@ -8,5 +8,9 @@ sudo yum install -y  git file gcc cmake perl
 echo "================== install brew for linux ============================"
 # prepare for non root user
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+echo "config brew automate"
+echo 'eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)' >> /home/hgf/.bash_profile
+eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 
+echo "=================== install common tools ============================="
 brew install jq exa httpie lrzsz minio-mc fzf fd lnav
